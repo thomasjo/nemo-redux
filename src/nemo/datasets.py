8d237c08e15fb2e7129a216c9e3a612d56e4743e
Thomas Johansen <thomasjo@gmail.com>
2020-08-14 08:48:14 +0200

    Simplify transform declarations

--- src/nemo/datasets.py
+++ src/nemo/datasets.py
@@ -10,20 +10,15 @@ from nemo.transforms import RandomDiscreteRotation
 def prepare_datasets(data_dir: Path):
     transform = Compose([
         ToTensor(),
-        # TODO(thomasjo): Re-enable normalization using computed dataset moments.
-        # Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        Normalize(mean=[0.232, 0.244, 0.269], std=[0.181, 0.182, 0.190]),
-        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        Normalize(mean=[0.232, 0.244, 0.269], std=[0.181, 0.182, 0.190]),  # Dataset
+        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # ImageNet
     ])
 
     augment_transform = Compose([
         ColorJitter(brightness=0.1, contrast=0.1, saturation=0.05, hue=0.05),
         RandomHorizontalFlip(),
         RandomDiscreteRotation(angles=[0, 90, 180, 270]),
-        ToTensor(),
-        # Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        Normalize(mean=[0.232, 0.244, 0.269], std=[0.181, 0.182, 0.190]),
-        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transform,
     ])
 
     train_dataset = ImageFolder(data_dir / "train", transform=augment_transform)
