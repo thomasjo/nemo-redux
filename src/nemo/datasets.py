83320d9167c4f5dc48eafd81ca0de8542746dcf3
Thomas Johansen <thomasjo@gmail.com>
2020-08-14 09:49:49 +0200

    Refactor dataset initialization yet again

--- src/nemo/datasets.py
+++ src/nemo/datasets.py
@@ -1,27 +1,34 @@
 from pathlib import Path
 
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import ColorJitter, Compose, RandomHorizontalFlip, ToTensor
-from torchvision.transforms import Normalize
+from torchvision.transforms import CenterCrop, ColorJitter, Compose, Normalize, RandomCrop, RandomHorizontalFlip, Resize, ToTensor
 
 from nemo.transforms import RandomDiscreteRotation
 
 
 def prepare_datasets(data_dir: Path):
+    # TODO(thomasjo): Read moments from file in data_dir or kwargs?
+    # moments = {"mean": [0.485, 0.456, 0.406], "std": [0.229, 0.224, 0.225]}  # ImageNet
+    moments = {"mean": [0.232, 0.244, 0.269], "std": [0.181, 0.182, 0.190]}  # Dataset
+
     transform = Compose([
+        Resize(256),
+        CenterCrop(224),
         ToTensor(),
-        Normalize(mean=[0.232, 0.244, 0.269], std=[0.181, 0.182, 0.190]),  # Dataset
-        # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # ImageNet
+        Normalize(**moments),
     ])
 
-    augment_transform = Compose([
-        ColorJitter(brightness=0.1, contrast=0.1, saturation=0.05, hue=0.05),
+    train_transform = Compose([
+        Resize(256),
+        RandomCrop(224),
         RandomHorizontalFlip(),
         RandomDiscreteRotation(angles=[0, 90, 180, 270]),
-        transform,
+        ColorJitter(brightness=0.1, contrast=0.1, saturation=0.05, hue=0.05),
+        ToTensor(),
+        Normalize(**moments),
     ])
 
-    train_dataset = ImageFolder(data_dir / "train", transform=augment_transform)
+    train_dataset = ImageFolder(data_dir / "train", transform=train_transform)
     val_dataset = ImageFolder(data_dir / "valid", transform=transform)
     test_dataset = ImageFolder(data_dir / "test", transform=transform)
 
