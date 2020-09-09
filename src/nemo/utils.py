50619866db8d76a8360897b3128835cc3a50e655
Thomas Johansen <thomasjo@gmail.com>
2020-09-09 17:54:30 +0200

    Implement "random state protection" context manager

--- src/nemo/utils.py
+++ src/nemo/utils.py
@@ -1,5 +1,6 @@
 import random
 
+from contextlib import ContextDecorator
 from datetime import datetime
 from pathlib import Path
 from types import ModuleType
@@ -19,12 +20,28 @@ def ensure_reproducibility(*, seed):
         torch.backends.cudnn.deterministic = True
 
 
+class random_state_protection(ContextDecorator):
+    def __enter__(self):
+        self.state = random.getstate()
+        self.numpy_state = numpy.random.get_state()
+        self.torch_state = torch.random.get_rng_state()
+
+        return self
+
+    def __exit__(self, *exc):
+        random.setstate(self.state)
+        numpy.random.set_state(self.numpy_state)
+        torch.random.set_rng_state(self.torch_state)
+
+        return self
+
+
 def ignore_warnings(module: ModuleType):
     module_name = module.__name__
     filterwarnings("ignore", module=f"{module_name}.*")
 
 
-def timestamped_path(path: Path):
+def timestamp_path(path: Path):
     timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
     new_path = path.parent / timestamp / path.name if path.is_file() else path / timestamp
     return new_path
