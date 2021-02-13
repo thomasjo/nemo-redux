582b984a55adae3091c8140c4802ac6d6f26cadd
Thomas Johansen <thomasjo@gmail.com>
2021-02-13 15:00:00 +0100

    Move detector model to models module

--- src/nemo/models.py
+++ src/nemo/models.py
@@ -1,7 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torchvision as vision
 
 from torchvision.models import vgg16_bn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, TwoMLPHead
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 
 class Classifier(nn.Module):
@@ -52,3 +56,45 @@ def initialize_classifier(num_classes):
     classifier = Classifier(feature_extractor, num_features, num_classes)
 
     return classifier
+
+
+class StochasticTwoMLPHead(TwoMLPHead):
+    def __init__(self, in_channels, representation_size, dropout_rate=0.2):
+        super().__init__(in_channels, representation_size)
+        self.dropout_rate = dropout_rate
+
+    def forward(self, x):
+        x = x.flatten(start_dim=1)
+
+        x = F.relu(self.fc6(x))
+        x = F.dropout(x, self.dropout_rate)
+        x = F.relu(self.fc7(x))
+        x = F.dropout(x, self.dropout_rate)
+
+        return x
+
+
+def initialize_detector(num_classes, dropout_rate=0):
+    # NOTE: See https://pytorch.org/docs/stable/torchvision/models.html#mask-r-cnn.
+    model = vision.models.detection.maskrcnn_resnet50_fpn(
+        pretrained=True,
+        box_detections_per_img=256,
+    )
+
+    # Use box head with dropout sampling support.
+    model.roi_heads.box_head = StochasticTwoMLPHead(
+        model.roi_heads.box_head.fc6.in_features,
+        model.roi_heads.box_head.fc7.out_features,
+        dropout_rate=dropout_rate,
+    )
+
+    # Customize box predictor.
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # Customize mask predictor.
+    hidden_layer = 256
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
+
+    return model
